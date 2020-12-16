103d028b91e0fc85585f7002c7026418bc2fd29a
Rob Leroy <robleroy@msn.com>
2020-12-16 14:47:27 -0600

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,3 @@
-#!/usr/bin/env sh
-
 # abort on errors
 set -e
 
@@ -12,7 +10,7 @@ cd dist
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
-git init
+# git init
 git add -A
 git commit -m 'deploy'
 
