3fef0c9a6031dced85e09c596a31ad0fca60e8d6
Rob Leroy <robleroy@msn.com>
2020-12-17 14:56:41 -0600

    update

--- vue.config.js
+++ vue.config.js
@@ -18,36 +18,36 @@ module.exports = {
     manifestOptions: {
       name: 'Conversion',
       short_name: 'Conversion',
-      description: 'Simple unit conversions',
+      // description: 'Simple unit conversions',
       start_url: '.',
       display: 'standalone',
       theme_color: '#000000',
       background_color: '#000000', 
-      icons: [
-        {
-          src: "./img/icons/android-chrome-192x192.png",
-          sizes: "192x192",
-          type: "image/png"
-        },
-        {
-          src: "./img/icons/android-chrome-512x512.png",
-          sizes: "512x512",
-          type: "image/png"
-        },
-        {
-          src: "./img/icons/android-chrome-maskable-192x192.png",
-          sizes: "192x192",
-          type: "image/png",
-          purpose: "maskable"
-        },
-        {
-          src: "./img/icons/android-chrome-maskable-512x512.png",
-          sizes: "512x512",
-          type: "image/png",
-          purpose: "maskable"
-        }
-      ],    
-    },
+      // icons: [
+      //   {
+      //     src: "./img/icons/android-chrome-192x192.png",
+      //     sizes: "192x192",
+      //     type: "image/png"
+      //   },
+      //   {
+      //     src: "./img/icons/android-chrome-512x512.png",
+      //     sizes: "512x512",
+      //     type: "image/png"
+      //   },
+      //   {
+      //     src: "./img/icons/android-chrome-maskable-192x192.png",
+      //     sizes: "192x192",
+      //     type: "image/png",
+      //     purpose: "maskable"
+      //   },
+      //   {
+      //     src: "./img/icons/android-chrome-maskable-512x512.png",
+      //     sizes: "512x512",
+      //     type: "image/png",
+      //     purpose: "maskable"
+      //   }
+      // ],    
+    // },
     iconPaths: {
       favicon32: 'img/icons/favicon-32x32.png',
       favicon16: 'img/icons/favicon-16x16.png',
